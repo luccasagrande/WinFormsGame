f2157e23e5a5cf6019f98c9495c95d3346f39da9
Lucas <luccasagrande@outlook.com>
2019-10-01 01:08:26 -0400

    change:
    rename form loading

--- SpeednSparks/Program.cs
+++ SpeednSparks/Program.cs
@@ -16,7 +16,7 @@ namespace SpeednSparks
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmLoading());
         }
     }
 }
