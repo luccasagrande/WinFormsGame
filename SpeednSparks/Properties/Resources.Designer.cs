b070670c892804cff121f985f3a8e1540477a662
Lucas <luccasagrande@outlook.com>
2019-10-01 02:06:34 -0400

    fix: remove duplicated background

--- SpeednSparks/Properties/Resources.Designer.cs
+++ SpeednSparks/Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace SpeednSparks.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap fromLoadingBG {
-            get {
-                object obj = ResourceManager.GetObject("fromLoadingBG", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap fromLoadingBG1 {
             get {
                 object obj = ResourceManager.GetObject("fromLoadingBG1", resourceCulture);
