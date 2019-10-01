d1b8359df8e9d5bcac9eebe8612c816bfeff433c
Lucas <luccasagrande@outlook.com>
2019-10-01 01:26:09 -0400

    style: resize form loading

--- SpeednSparks/frmLoading.Designer.cs
+++ SpeednSparks/frmLoading.Designer.cs
@@ -34,7 +34,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(720, 410);
+            this.BackgroundImage = global::SpeednSparks.Properties.Resources.fromLoadingBG;
+            this.ClientSize = new System.Drawing.Size(720, 404);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmLoading";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
